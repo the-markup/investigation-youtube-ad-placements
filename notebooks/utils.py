112e58a7ead20cc8dd7e3a247cefec3c51550859
leon yin <leon.yin@themarkup.org>
2021-03-18 12:05:37 -0400

    added ribeiro list back in

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
 def value_counts(df: pd.DataFrame, 
-                 col: str, **args) -> pd.DataFrame:
+                 col: str, 
+                 *args. **kwargs) -> pd.DataFrame:
     """
     For a DataFrame (`df`): display normalized (percentage) 
     `value_counts(normalize=True)` and regular counts 
     `value_counts()` for a given `col`.
     """
-    count = df[col].value_counts(**args).to_frame(name='count')
-    perc = df[col].value_counts(normalize=True, **args) \
+    count = df[col].value_counts(*args, **kwargs).to_frame(name='count')
+    perc = df[col].value_counts(normalize=True, *args,**kwargs) \
                   .to_frame(name='percentage')
     
-    return count.T.append(perc.T).T 
+    return pd.concat([count, perc], axis=1)
